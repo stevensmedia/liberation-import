53dda575f59a624ee675aeda0416f9cd04dc0bc6
Neil Stevens <neil@stevensmedia.us>
2020-09-27 14:06:21 -0400

    Load html files and get ready to process them

--- liberation-import.js
+++ liberation-import.js
@@ -1,4 +1,5 @@
 const cheerio = require('cheerio');
+const fs = require('fs').promises;
 const glob = require('glob');
 
 function processArticle(html) {
@@ -7,23 +8,42 @@ function processArticle(html) {
 
 function getFiles(globstr) {
 	return new Promise(function(resolve, reject) {
-		glob('*.html', {}, function(error, files) {
+		glob(globstr, {}, function(error, files) {
 			if(error) {
 				reject(error);
-			} else 
+			} else {
 				resolve(files);
 			}
-		}
+		});
 	});
 }
 
 async function main() {
+	var target = process.argv.slice(-1)[0];
+
 	try {
-		var files = getFiles('*.html');
+		var files = await getFiles(target);
 	} catch(e) {
 		console.log(`Error getting files: ${e}`);
 		return;
 	}
+
+	files.forEach(async function(file) {
+		var html = false;
+		try {
+			html = await fs.readFile(file);
+		} catch(e) {
+			console.log(`Error getting file ${file}: ${e}`);
+			return;
+		}
+
+		try {
+			var processed = await processArticle(html);
+		} catch(e) {
+			console.log(`Error processing file ${file}: ${e}`);
+			return;
+		}
+	});
 }
 
 main();
